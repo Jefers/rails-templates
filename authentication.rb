39db76a5adb51b9bc9ef05afe52e4a7090041d6f
Jeff Gordon <m.j.l.gordon@googlemail.com>
2009-11-16 16:59:48 +0000

    add in the first templates

---
+++ authentication.rb
@@ -0,0 +1,13 @@
+load_template "http://github.com/Jefers/rails-templates/raw/master/base.rb"
+
+name = ask("what do you want a user to be called?")
+generate :nifty_authentication, name
+rake "db:migrate"
+
+git :add => ".",  :commit => "-m 'adding authentication"
+
+generate :controller, "welcome index"
+route "map.root :controller => 'welcome'"
+git :rm => "public/index.html"
+
+git :add => ".", :commit => "-m 'adding welcome controller"
