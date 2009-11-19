4ff850f13e7e5893e28bbcafb054f27cd90c5e90
Jefers <m.j.l.gordon@googlemail.com>
2009-11-19 09:06:48 -0700

    Added attachment_fu and paperclip plugins

--- base.rb
+++ base.rb
@@ -6,11 +6,19 @@ gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.git
 rake "gems:install"
 
  
+if yes?("Do you want to use attachment_fu?")
+  plugin "attachment_fu", :git => "git://github.com/technoweenie/attachment_fu.git"
+end
+ 
+ 
+if yes?("Do you want to use Paperclip?")
+  plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
+end
+  
 if yes?("Do you want to use tagging?")
   plugin "acts-as-taggable-on", :git => "git://github.com/mbleigh/acts-as-taggable-on.git"
 end
  
-
 if yes?("Do you want to use RSpec?")
   plugin "rspec", :git => "git://github.com/dchelimsky/rpec.git"
   plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec.rails.git"
