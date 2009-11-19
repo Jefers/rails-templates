e7af6a8efcffdd6825d91e1e11027056100ee3c5
Jefers <m.j.l.gordon@googlemail.com>
2009-11-19 12:01:47 -0700

    fix business

--- authentication.rb
+++ authentication.rb
@@ -2,7 +2,7 @@ load_template "http://github.com/Jefers/rails-templates/raw/master/base.rb"
 
 generate :nifty_scaffold, "Contact salutation_id:integer  first_name:string initials:string surname:string mobile:string home:string email:string address:string address_2:string address_3:string town:string county:string postal_code:string country_id:integer"
 
-generate :nifty_scaffold, "Business name:string strap:string description:text street:string town:string open_times:string phone:string fax:string mobile:string website:string contact_id email:string address:string address_2:string address_3:string city:string county:string postal_code:string country_id:integer"
+generate :nifty_scaffold, "Business name:string strap:string description:text street:string town:string open_times:string phone:string fax:string mobile:string website:string contact_id:integer email:string address:string address_2:string address_3:string city:string county:string postal_code:string country_id:integer"
 
 generate :model, "Mugshot parent_id:integer contact_id:integer content_type:string filename:string thumbnail:string size:integer width:integer height:integer"
 
