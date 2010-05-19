c0fd2f23b1c240e36dc021708e6203909715e30c
Jefers <m.j.l.gordon@googlemail.com>
2010-05-19 05:19:38 -0700

    moved other stuff

--- authentication.rb
+++ authentication.rb
@@ -1,13 +1,4 @@
 
-
 name = ask("what do you want a user to be called?")
 generate :nifty_authentication, name
-rake "db:migrate"
 
-git :add => ".",  :commit => "-m 'adding authentication'"
-
-generate :controller, "welcome index"
-route "map.root :controller => 'welcome'"
-git :rm => "public/index.html"
-
-git :add => ".", :commit => "-m 'adding welcome controller'"
