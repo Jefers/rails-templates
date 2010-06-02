9e00cf4a0c6ec56031318549174aef54c636ccf1
Jefers <m.j.l.gordon@googlemail.com>
2010-06-02 02:47:58 -0700

    minus compass etc and add searchlogic changed order of categories to be before listings changed listings to have category_id and cat1

--- base.rb
+++ base.rb
@@ -2,9 +2,9 @@ generate :nifty_scaffold, "Contact salutation_id:integer  first_name:string init
 
 generate :nifty_scaffold, "Business name:string strap:string description:text street:string town:string open_times:string phone:string fax:string mobile:string website:string contact_id:integer email:string address:string address_2:string address_3:string city:string county:string postal_code:string country_id:integer"
 
-generate :nifty_scaffold, "Listing name:string url:string phone:string address:string line2:string line3:string line4:string line5:string county:string post_code:string category:string cat2:string cat3:string cat4:string cat5:string cat6:string cat7:string cat8:string cat9:string cat10:string cat11:string cat12:string"
-
 generate :nifty_scaffold, "Category name:string description:text"
+
+generate :nifty_scaffold, "Listing name:string url:string phone:string address:string line2:string line3:string line4:string line5:string county:string post_code:string category_id:integer cat1:string cat2:string cat3:string cat4:string cat5:string cat6:string cat7:string cat8:string cat9:string cat10:string cat11:string cat12:string"
 
 generate :model, "Mugshot parent_id:integer contact_id:integer content_type:string filename:string thumbnail:string size:integer width:integer height:integer"
 
@@ -20,10 +20,12 @@ run "echo TODO > README"
 generate :nifty_layout
 
 gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-gem 'fancy-buttons', :version => '>=0.5.3'
-gem 'compass', :version => '>= 0.10.1'
-gem 'haml', :version => '>=3.0.4'
+#gem 'fancy-buttons', :version => '>=0.5.3'
+#gem 'compass', :version => '>= 0.10.1'
+#gem 'haml', :version => '>=3.0.4'
+gem 'searchlogic', :version => '>=2.4.19'
 gem 'formtastic', :version => '>=0.9.8'
+gem 'repeated_auto_complete'
 rake('gems:install', :sudo => true)
 rake "gems:unpack:dependencies"
  
