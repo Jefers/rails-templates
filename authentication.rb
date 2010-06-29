ac70907b1a42c07504fb5b9adf074f255697b5cf
Jefers <m.j.l.gordon@googlemail.com>
2010-06-29 12:00:23 -0700

    Add Devise

--- authentication.rb
+++ authentication.rb
@@ -1,4 +1,14 @@
+if yes?("Do you want to use Devise?")
+ 
+generate "devise User"
 
+file "config/environments/development.rb", <<-END
+
+config.action_mailer.default_url_options = { :host => 'localhost:3010' }
+
+END
+
+else
 name = ask("what do you want a user to be called?")
 generate :nifty_authentication, name
-
+end
