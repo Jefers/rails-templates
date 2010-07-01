10bf3470bd88e2f031ba7423f42280fc10a825aa
Jefers <m.j.l.gordon@googlemail.com>
2010-06-30 23:19:56 -0700

    development.rb file

--- authentication.rb
+++ authentication.rb
@@ -2,11 +2,16 @@ if yes?("Do you want to use Devise?")
  
 generate "devise User"
 
-file "config/environments/development.rb", <<-END
-
+file "config/environments/development.rb", <<-DEV
+config.cache_classes = false
+config.whiny_nils = true
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default_url_options = { :host => 'localhost:3010' }
 
-END
+DEV
 
 else
 name = ask("what do you want a user to be called?")
