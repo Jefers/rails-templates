71c48ee3a7d9417e9f9a1eb1f7ba37605dced354
Jefers <m.j.l.gordon@googlemail.com>
2010-08-24 00:56:03 -0700

    add roles

--- authentication.rb
+++ authentication.rb
@@ -1,6 +1,9 @@
 if yes?("Do you want to use Devise?")
  
 generate "devise User"
+generate "devise:views"
+generate :nifty_scaffold, "Role name:string"
+generate :migration "create_roles_user_table role_id:integer user_id:integer"
 
 file "config/environments/development.rb", <<-DEV
 config.cache_classes = false
